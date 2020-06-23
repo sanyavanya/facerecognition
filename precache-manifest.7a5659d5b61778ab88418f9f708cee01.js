f3085de380948c848480fc2999643c2db02a436f
sanyavanya <ivanov.unn@gmail.com>
2020-06-23 15:01:02 +0300

    Updates

---
+++ precache-manifest.7a5659d5b61778ab88418f9f708cee01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f14e88af7769c063828b6a5e1856b8",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "3e844aedc0a4af05d67a",
+    "url": "/facerecognition/static/css/2.20355f55.chunk.css"
+  },
+  {
+    "revision": "b5017e9ef5fa6717898b",
+    "url": "/facerecognition/static/css/main.9ab7c591.chunk.css"
+  },
+  {
+    "revision": "3e844aedc0a4af05d67a",
+    "url": "/facerecognition/static/js/2.814d868e.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/facerecognition/static/js/2.814d868e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5017e9ef5fa6717898b",
+    "url": "/facerecognition/static/js/main.30a54b2f.chunk.js"
+  },
+  {
+    "revision": "6605f218dc406a018577",
+    "url": "/facerecognition/static/js/runtime-main.b29f0c47.js"
+  }
+]);
